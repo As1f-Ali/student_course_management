55d2e75a9b60b73c187b96bcd13d34dda5d92065
Asif Ali <asifali94851@gmail.com>
2026-07-09 21:11:03 +0530

    Implement live search

--- js/main.js
+++ js/main.js
@@ -2,8 +2,29 @@ import { fetchStudents } from "./api.js";
 import { displayStudents } from "./ui.js";
 
 const init = async () => {
+
     const students = await fetchStudents();
 
     displayStudents(students);
+
+    const searchInput = document.getElementById("search-input");
+
+    searchInput.addEventListener("input", () => {
+
+        const searchText = searchInput.value;
+
+        const filteredStudents = students.filter(student => {
+
+            return student.name
+                .toLowerCase()
+                .includes(searchText.toLowerCase());
+
+        });
+
+        displayStudents(filteredStudents);
+
+    });
+
 };
+
 init();
